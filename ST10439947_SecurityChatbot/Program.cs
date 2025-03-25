cc8c02ea80bbed3b909577aa679421ed8e9b8d88
Atlas <ST10439947@vcconnect.edu.za>
2025-03-25 15:23:45 +0200

    Testing CI

--- ST10439947_SecurityChatbot/Program.cs
+++ ST10439947_SecurityChatbot/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿//==================================================================================/
+/*
+ * Amelia Swart
+ * ST10439947
+ * POE - Part 1: Security Chatbot
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//---------------------------------------------------------------------------------/
 namespace ST10439947_SecurityChatbot
 {
     class Program
     {
+//---------------------------------------------------------------------------------/
         static void Main(string[] args)
         {
         }
     }
 }
+//==================================================================================/
